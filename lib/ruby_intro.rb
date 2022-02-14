9c370f1e54c3756fb03d3e22121427e13fc8cbc6
Ubuntu <ubuntu@ip-172-31-50-40.ec2.internal>
2022-02-14 04:08:01 +0000

    HW1 - finished

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,56 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  ans = 0
+  arr.each{|x| ans = x + ans}
+  ans
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  arr.sort.reverse.take(2).sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.find{|x| 
+  tmp = arr
+  tmp.delete_at(tmp.index(x))
+  if tmp.include?(n-x) 
+    return true
+  end
+  }
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name.to_str
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  
+  if s[0] and s.capitalize.scan(/[AEIOU]/).count == 0 and s[0].match(/^[[:alpha:]]$/)
+    return true
+  end
+  return false
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  s.length>0 and s !~ /[^01]/ and s.to_i()%4==0
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn,price)
+    @isbn = isbn
+    @price = price 
+    if (@isbn == "" || @price <= 0) #Exception handling
+      raise ArgumentError
+    end
+  end
+  def price_as_string #price processing
+    "$" + "%.2f" % @price
+  end
 end
